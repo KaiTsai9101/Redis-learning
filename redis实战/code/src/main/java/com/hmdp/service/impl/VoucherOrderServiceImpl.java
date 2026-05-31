6de043cd768d5f0e111d2bfb8c9d3a60c743ccb0
Kai Tsai <105984936+KaiTsai9101@users.noreply.github.com>
2026-05-31 23:51:08 +0800

    Add files via upload

--- redis实战/code/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ redis实战/code/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,20 +1,99 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import lombok.NonNull;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
  *  服务实现类
  * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        // 1.查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2.判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // 尚未开始
+            return Result.fail("秒杀尚未开始");
+        }
+        // 3.判断秒杀是否结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            // 已经结束
+            return Result.fail("秒杀已经结束");
+        }
+        // 4.判断库存是否充足
+        if (voucher.getStock() < 1) {
+            // 库存不足
+            return Result.fail("库存不足");
+        }
+
+        // 5.一人一单
+        Long userId = UserHolder.getUser().getId();
+        // 锁放在这里而不是createVoucherOrder方法内
+        // 原因：方法执行完后就释放锁了，但此时数据由spring提交，期间如果其他线程获取锁，则无法查询到订单记录，从而导致并发安全问题（仍可重复购买优惠券）
+        synchronized(userId.toString().intern()) {
+            // 获取代理对象（事务）（Spring只能给代理对象创建事务Transactional，不能给目标对象this）
+            // AopContext.currentProxy() 的功能和getBean()一样，都是获取bean对象
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        Long userId = UserHolder.getUser().getId();
+        // 锁库存（由于toString()是将userId转换成全新对象，所以这里还需要intern()将该对象转换成与userId一样的值（返回字符串对象的规范表示））
+
+        // 5.1.查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // 5.2.判断是否存在
+        if (count > 0) {
+            // 用户已经购买过了
+            return Result.fail("用户已经购买过一次");
+        }
+        // 6.扣减库存（MyBatisPlus的条件构造器）
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")        // set stock = stock - 1
+                .eq("voucher_id", voucherId)        // where voucher_id = ?
+                .gt("stock", 0)         // and stock > 0
+                .update();
+        if (!success) {
+            // 扣减失败
+            return Result.fail("库存不足");
+        }
+        // 7.创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();// 7.1.订单ID
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        // 7.2.用户ID
+        voucherOrder.setUserId(userId);
+        // 7.3.代金券ID
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        // 8.返回订单结果
+        return Result.ok(orderId);
+    }
 }
--- redis实战/code/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ redis实战/code/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -2,11 +2,15 @@ package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -19,6 +23,35 @@ class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        // 设置线程数
+        CountDownLatch latch = new CountDownLatch(300);
+
+        // 每个线程执行100次id生成
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            // 生成完成后减去该线程
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        // 提交任务
+        for (int i = 0; i < 300; i++) {
+            executorService.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 
     @Test
     void testSaveShop() throws InterruptedException {
