04b50a6f90281cba6e81e7070d5e49f27b7a1773
Kai Tsai <105984936+KaiTsai9101@users.noreply.github.com>
2026-06-03 23:52:48 +0800

    Add files via upload

--- redis实战/code/src/main/resources/seckill.lua
+++ redis实战/code/src/main/resources/seckill.lua
@@ -3,6 +3,9 @@
 local voucherId = ARGV[1]
 -- 1.2.用户id
 local userId = ARGV[2]
+-- 1.3.订单id
+local orderId = ARGV[3]
+
 
 -- 2.数据key
 -- 2.1.库存key（lua中拼接不是用 + 而是用 ..）
@@ -27,6 +30,7 @@ end
 redis.call('incrby', stockKey, -1)
 -- 3.5.下单（记录用户信息） sadd orderKey userId
 redis.call('sadd', orderKey, userId)
-
+-- 3.6.发送消息到队列中， XADD stream.orders * k1 v1 k2 v2 ...
+redis.call('xadd', 'stream.orders', '*', 'userId', userId, 'voucherId', voucherId, 'id', orderId)
 -- 4.成功返回0
 return 0
