98b5c92d567ae95bfe1364ba83ba043032b3cf74
Kai Tsai <105984936+KaiTsai9101@users.noreply.github.com>
2026-06-04 23:34:25 +0800

    Add files via upload

--- redis实战/code/src/main/java/com/hmdp/service/IBlogService.java
+++ redis实战/code/src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -10,4 +11,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    Result queryHotBlog(Integer current);
+
+    Result queryBlogById(Long id);
+
+    Result likeBog(Long id);
+
+    Result queryBlogLikes(Long id);
 }
